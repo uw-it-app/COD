9565ab2c28e4d69084b16fa9dc442a39630656b9
jobywalker <joby@imperium.org>
2011-12-08 13:36:12 -0800

    JSLint

--- web/js/cod.js
+++ web/js/cod.js
@@ -1,41 +1,48 @@
+/*jslint nomen: true, regexp: true */
+/*global $ */
+/*global logger */
+/*global RESTDataSource */
+/*global badgerArray */
 (function () {
     'use strict';
+    $(function () {
 
-$(function () {
+        var COD = {};
 
-    // Datasource locations
-    COD.dataSources = {
-        "items": "/daw/json/COD/v2/Items"
-    };
+        // Datasource locations
+        COD.dataSources = {
+            "items": "/daw/json/COD/v2/Items"
+        };
 
-    // Namespace for JSON data
-    COD.data = {};
+        // Namespace for JSON data
+        COD.data = {};
 
-    // RESTDatasource Error Handler
-    COD.RESTErrorHandler = function(XMLHttpRequest, textStatus, errorThrown) {
-        logger.debug(errorThrown);
-    };
+        // RESTDatasource Error Handler
+        COD.RESTErrorHandler = function (XMLHttpRequest, textStatus, errorThrown) {
+            logger.debug(errorThrown);
+        };
 
-    // REST DataSources
-    COD.REST = {};
-    COD.REST.items = new RESTDataSource(COD.dataSources.items, COD.RESTErrorHandler);
-    
-    
-    COD.itemsload = function(data){
-        data.Items.Item = badgerArray(data.Items.Item);
-        var count = data.Items.Item.length;
-        for (var i = 0; i < count; i++) {
-            data.Items.Item[i].Escalations.Escalation = badgerArray(data.Items.Item[i].Escalations.Escalation);
-        }
-        COD.data.items = data;
-        $('.items_bind').jpop(COD.data.items, {});
-    }
-    
-    COD.start = function(){
-        COD.REST.items.get({}, COD.itemsload, null);
-    }
-    COD.start();
-});
+        // REST DataSources
+        COD.REST = {};
+        COD.REST.items = new RESTDataSource(COD.dataSources.items, COD.RESTErrorHandler);
+
+        COD.itemsload = function (data) {
+            var count, i;
+            data.Items.Item = badgerArray(data.Items.Item);
+            count = data.Items.Item.length;
+            for (i = 0; i < count; i = i + 1) {
+                data.Items.Item[i].Escalations.Escalation = badgerArray(data.Items.Item[i].Escalations.Escalation);
+            }
+            COD.data.items = data;
+            $('.items_bind').jpop(COD.data.items, {});
+        };
+
+        COD.start = function () {
+            COD.REST.items.get({}, COD.itemsload, null);
+        };
+
+        COD.start();
+    });
 }());
 
 
