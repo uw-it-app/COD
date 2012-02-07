120ac26aa37d5f71a0ac25a461002618d95229d8
jobywalker <joby@imperium.org>
2012-02-07 10:27:59 -0800

    HM db changes/new functions

--- sql/hm_v1_00.sql
+++ sql/hm_v1_00.sql
@@ -17,3 +17,66 @@ CREATE OR REPLACE FUNCTION hm_v1.get_oncall_queue(varchar) RETURNS varchar
 $_$;
 
 COMMENT ON FUNCTION hm_v1.get_oncall_queue(varchar) IS 'DR: Retrieve the RT queue associated with an oncall group (2011-11-16)';
+
+-- create issue fn
+    -- create issue
+    -- create first squawk
+    -- notify RT
+/**********************************************************************************************/
+
+CREATE OR REPLACE FUNCTION hm_v1.create_issue(xml) RETURNS integer
+    LANGUAGE plpgsql
+    VOLATILE
+    SECURITY INVOKER
+    AS $_$
+/*  Function:     hm_v1.create_issue(xml)
+    Description:  Function to create a notification
+    Affects:      Inserts hm_issue and associated sqawk
+    Arguments:    xml: XML document describing the new Issue
+    Returns:      integer
+*/
+DECLARE
+    v_xml       ALIAS FOR $1;
+    _id         integer;
+BEGIN
+    -- oncall group
+    -- ticket
+    -- subject
+    -- message
+    -- shortmessage
+    -- owner = 'nobody'
+    -- switch -- ignore for now
+    -- Active = true
+    -- comment
+    -- origin
+
+
+    -- createSquawk or done by cleanup in hm's Act???
+    -- NOTIFY RT
+    -- return id of issue
+--EXCEPTION
+--    WHEN OTHERS THEN null;
+END;
+$_$;
+
+COMMENT ON FUNCTION hm_v1.create_issue(xml) IS 'DR: Function to create a notification (2012-02-06)';
+
+
+--
+-- update COD
+--
+
+-- have squawk update update modtime of issue
+-- on issue update COD via trigger with:
+    -- state
+    -- owner
+    -- current squawk (name, contact data)
+
+-- failed notification
+    -- prompt operator to contact duty manageer
+
+-- current method so 
+    -- if phone call create action to perform phone call
+    -- else cancel phone call action
+
+-- success -- set owner
