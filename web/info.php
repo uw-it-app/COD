70b3a22853f0381eb80f1a3ecce586dbd51b2ed2
jobywalker <joby@imperium.org>
2012-02-14 10:38:32 -0800

    Force https

--- web/info.php
+++ web/info.php
@@ -15,3 +15,9 @@ function pathinfoToHash () {
 	}
 	return $out;
 }
+
+# redirect to https if http
+if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+	header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], true, 301);
+	exit();
+}
