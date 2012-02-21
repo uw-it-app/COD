918a366d78cd5102cde0a572bd56fef087f6906a
jobywalker <joby@imperium.org>
2012-02-21 09:27:43 -0800

    Use finalized tile 0.0.6

--- web/js.php
+++ web/js.php
@@ -8,8 +8,7 @@
         <script src="/shared/local/core/4.4.8/tools-core.min.js"></script>
         <script src="/shared/local/jquery.jpop/1.0.3/jquery.jpop.js"></script>
         <script src="/shared/local/badgerfishjs/1.0.0/badgerfish.js"></script>
-        <script src="/shared/local/jquery.ui.tile/0.0.6-dev/jquery.ui.tile.js"></script>
-        <!--<script src="/.cod/js/jquery.ui.tile.js"></script>-->
+        <script src="/shared/local/jquery.ui.tile/0.0.6/jquery.ui.tile.js"></script>
         <script src="/.cod/js/RESTDataSource.js"></script>
         <!--
         <script src="/shared/third-party/underscore/1.1.6/underscore-min.js" ></script>
