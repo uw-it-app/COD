6ec4780411ed7d08cd0b15665187f22ca5eb65f2
jobywalker <joby@imperium.org>
2012-02-25 10:50:34 -0800

    Enumerations for UI

---
+++ sql/cod_v2_02.sql
@@ -0,0 +1,27 @@
+/**********************************************************************************************/
+
+CREATE OR REPLACE FUNCTION cod_v2.enumerations() RETURNS xml
+    LANGUAGE sql
+    STABLE
+    SECURITY INVOKER
+    AS $_$
+/*  Function:     cod_v2.enumerations()
+    Description:  Return lists of enumeration data for selects
+    Affects:      nothing
+    Arguments:    none
+    Returns:      xml
+*/
+SELECT xmlelement(name "Eumerations"
+    , rest_v1.enum_to_xml('cod', 'support_model', 'SupportModels', 'SupportModel', false)
+    , rest_v1.enum_to_xml('cod', 'itil_type', 'ITILTypes', 'ITILType', false)
+    , xmlelement(name "Severities"
+        , xmlelement(name "Severity", 1)
+        , xmlelement(name "Severity", 2)
+        , xmlelement(name "Severity", 3)
+        , xmlelement(name "Severity", 4)
+        , xmlelement(name "Severity", 5)
+    )
+);
+$_$;
+
+COMMENT ON FUNCTION cod_v2.enumerations() IS 'DR: Return lists of enumeration data for selects (2012-02-25)';
