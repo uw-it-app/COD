09a3eedfb3205b601b9f0b6b62a4f6193b843d90
jobywalker <joby@imperium.org>
2012-02-29 21:09:17 -0800

    v2.1.0

--- web/info.php
+++ web/info.php
@@ -1,26 +1,26 @@
 <?php
-$version = '2.0.0-beta6';
+$version = '2.1.0';
 
 /****************************************************************************/
 
 # redirect to https if http
 if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
-	header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], true, 301);
-	exit();
+    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], true, 301);
+    exit();
 }
 header('X-UA-Compatible: IE=Edge,chrome=1');
 
 function pathinfoToHash () {
-	$pathinfo = $_SERVER['PATH_INFO'];
-	$split = mb_split('/', $pathinfo);
-	$out = array();
-	for ($x=1; $x<count($split); $x+=2) {
-		$key   = preg_replace('/[^a-z0-9_\-]/i', '', urldecode($split[$x]));
-		if (isset($split[$x+1])) {
-			$out[$key] = preg_replace('/[^a-z0-9_\-]/i', '', urldecode($split[$x+1]));
-		} else {
-			$out[$key] = NULL;
-		}
-	}
-	return $out;
+    $pathinfo = $_SERVER['PATH_INFO'];
+    $split = mb_split('/', $pathinfo);
+    $out = array();
+    for ($x=1; $x<count($split); $x+=2) {
+        $key   = preg_replace('/[^a-z0-9_\-]/i', '', urldecode($split[$x]));
+        if (isset($split[$x+1])) {
+            $out[$key] = preg_replace('/[^a-z0-9_\-]/i', '', urldecode($split[$x+1]));
+        } else {
+            $out[$key] = NULL;
+        }
+    }
+    return $out;
 }
